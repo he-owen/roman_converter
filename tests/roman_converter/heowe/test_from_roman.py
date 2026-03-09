807be2e0eeb9a8d991fd56b7a942e9a5283f5afe
he-owen <heowen@udel.edu>
2026-03-09 15:46:39 -0400

    separate tests

--- tests/roman_converter/heowe/test_from_roman.py
+++ tests/roman_converter/heowe/test_from_roman.py
@@ -2,50 +2,125 @@ from types import ModuleType
 
 
 # Basic numerals
-def test_basic_numerals(impl: ModuleType):
+def test_I(impl: ModuleType):
     assert impl.from_roman("I") == 1
+
+
+def test_V(impl: ModuleType):
     assert impl.from_roman("V") == 5
+
+
+def test_X(impl: ModuleType):
     assert impl.from_roman("X") == 10
+
+
+def test_L(impl: ModuleType):
     assert impl.from_roman("L") == 50
+
+
+def test_C(impl: ModuleType):
     assert impl.from_roman("C") == 100
+
+
+def test_D(impl: ModuleType):
     assert impl.from_roman("D") == 500
+
+
+def test_M(impl: ModuleType):
     assert impl.from_roman("M") == 1000
 
 
 # Subtractive notation
-def test_subtractive_notation(impl: ModuleType):
+def test_IV(impl: ModuleType):
     assert impl.from_roman("IV") == 4
+
+
+def test_IX(impl: ModuleType):
     assert impl.from_roman("IX") == 9
+
+
+def test_XL(impl: ModuleType):
     assert impl.from_roman("XL") == 40
+
+
+def test_XC(impl: ModuleType):
     assert impl.from_roman("XC") == 90
+
+
+def test_CD(impl: ModuleType):
     assert impl.from_roman("CD") == 400
+
+
+def test_CM(impl: ModuleType):
     assert impl.from_roman("CM") == 900
 
 
 # Repetition
-def test_repetition(impl: ModuleType):
+def test_II(impl: ModuleType):
     assert impl.from_roman("II") == 2
+
+
+def test_III(impl: ModuleType):
     assert impl.from_roman("III") == 3
+
+
+def test_XX(impl: ModuleType):
     assert impl.from_roman("XX") == 20
+
+
+def test_XXX(impl: ModuleType):
     assert impl.from_roman("XXX") == 30
+
+
+def test_CCC(impl: ModuleType):
     assert impl.from_roman("CCC") == 300
+
+
+def test_MMM(impl: ModuleType):
     assert impl.from_roman("MMM") == 3000
 
 
 # Compound numerals
-def test_compound_numerals(impl: ModuleType):
+def test_XIV(impl: ModuleType):
     assert impl.from_roman("XIV") == 14
+
+
+def test_XLII(impl: ModuleType):
     assert impl.from_roman("XLII") == 42
+
+
+def test_XCIX(impl: ModuleType):
     assert impl.from_roman("XCIX") == 99
+
+
+def test_CCCXCIX(impl: ModuleType):
     assert impl.from_roman("CCCXCIX") == 399
+
+
+def test_MCMXCIV(impl: ModuleType):
     assert impl.from_roman("MCMXCIV") == 1994
+
+
+def test_MMXXIV(impl: ModuleType):
     assert impl.from_roman("MMXXIV") == 2024
+
+
+def test_MMMCMXCIX(impl: ModuleType):
     assert impl.from_roman("MMMCMXCIX") == 3999
 
 
 # Misc values
-def test_misc_values(impl: ModuleType):
+def test_LVIII(impl: ModuleType):
     assert impl.from_roman("LVIII") == 58
+
+
+def test_DCXXI(impl: ModuleType):
     assert impl.from_roman("DCXXI") == 621
+
+
+def test_MDCCLXXVI(impl: ModuleType):
     assert impl.from_roman("MDCCLXXVI") == 1776
+
+
+def test_MMCDXXI(impl: ModuleType):
     assert impl.from_roman("MMCDXXI") == 2421
--- tests/roman_converter/heowe/test_to_roman.py
+++ tests/roman_converter/heowe/test_to_roman.py
@@ -2,50 +2,125 @@ from types import ModuleType
 
 
 # Basic numerals
-def test_basic_numerals(impl: ModuleType):
+def test_1(impl: ModuleType):
     assert impl.to_roman(1) == "I"
+
+
+def test_5(impl: ModuleType):
     assert impl.to_roman(5) == "V"
+
+
+def test_10(impl: ModuleType):
     assert impl.to_roman(10) == "X"
+
+
+def test_50(impl: ModuleType):
     assert impl.to_roman(50) == "L"
+
+
+def test_100(impl: ModuleType):
     assert impl.to_roman(100) == "C"
+
+
+def test_500(impl: ModuleType):
     assert impl.to_roman(500) == "D"
+
+
+def test_1000(impl: ModuleType):
     assert impl.to_roman(1000) == "M"
 
 
 # Subtractive notation
-def test_subtractive_notation(impl: ModuleType):
+def test_4(impl: ModuleType):
     assert impl.to_roman(4) == "IV"
+
+
+def test_9(impl: ModuleType):
     assert impl.to_roman(9) == "IX"
+
+
+def test_40(impl: ModuleType):
     assert impl.to_roman(40) == "XL"
+
+
+def test_90(impl: ModuleType):
     assert impl.to_roman(90) == "XC"
+
+
+def test_400(impl: ModuleType):
     assert impl.to_roman(400) == "CD"
+
+
+def test_900(impl: ModuleType):
     assert impl.to_roman(900) == "CM"
 
 
 # Repetition
-def test_repetition(impl: ModuleType):
+def test_2(impl: ModuleType):
     assert impl.to_roman(2) == "II"
+
+
+def test_3(impl: ModuleType):
     assert impl.to_roman(3) == "III"
+
+
+def test_20(impl: ModuleType):
     assert impl.to_roman(20) == "XX"
+
+
+def test_30(impl: ModuleType):
     assert impl.to_roman(30) == "XXX"
+
+
+def test_300(impl: ModuleType):
     assert impl.to_roman(300) == "CCC"
+
+
+def test_3000(impl: ModuleType):
     assert impl.to_roman(3000) == "MMM"
 
 
 # Compound numerals
-def test_compound_numerals(impl: ModuleType):
+def test_14(impl: ModuleType):
     assert impl.to_roman(14) == "XIV"
+
+
+def test_42(impl: ModuleType):
     assert impl.to_roman(42) == "XLII"
+
+
+def test_99(impl: ModuleType):
     assert impl.to_roman(99) == "XCIX"
+
+
+def test_399(impl: ModuleType):
     assert impl.to_roman(399) == "CCCXCIX"
+
+
+def test_1994(impl: ModuleType):
     assert impl.to_roman(1994) == "MCMXCIV"
+
+
+def test_2024(impl: ModuleType):
     assert impl.to_roman(2024) == "MMXXIV"
+
+
+def test_3999(impl: ModuleType):
     assert impl.to_roman(3999) == "MMMCMXCIX"
 
 
 # Misc values
-def test_misc_values(impl: ModuleType):
+def test_58(impl: ModuleType):
     assert impl.to_roman(58) == "LVIII"
+
+
+def test_621(impl: ModuleType):
     assert impl.to_roman(621) == "DCXXI"
+
+
+def test_1776(impl: ModuleType):
     assert impl.to_roman(1776) == "MDCCLXXVI"
+
+
+def test_2421(impl: ModuleType):
     assert impl.to_roman(2421) == "MMCDXXI"
